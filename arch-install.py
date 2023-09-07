e67262f54d95253e4c4083bbd8de8b01d3f6aee6
Eliminater74 <eliminater74@gmail.com>
2023-09-07 14:35:56 -0400

    REM: small note

--- arch-install.py
+++ arch-install.py
@@ -73,6 +73,7 @@ def display_menu(stdscr):
 
     curses.curs_set(0)
     stdscr.keypad(1)  # Enable special keys
+    # Initialize color pairs for menu highlighting
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
 
