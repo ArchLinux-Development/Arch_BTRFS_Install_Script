4ee83630a89f262913a2472450dcda34665efe41
Eliminater74 <eliminater74@gmail.com>
2023-09-07 16:12:39 -0400

    menu: fix up some unfinished sections as well

--- arch-install.py
+++ arch-install.py
@@ -60,108 +60,73 @@ def display_intro():
     input("\nPress Enter to proceed to the main menu...")
 
 
+import curses
+
 def display_menu(stdscr):
-    menu_items = [
-        "Install Filesystem", "Install essential packages", "Configure fstab",
-        "Chroot into system", "Set time zone", "Localization", "Network configuration",
-        "Set hostname", "Set root password", "Create a new user", "Kernel Selector",
-        "Install additional packages", "Install custom packages", "Desktop Environment Installation",
-        "Enable necessary services", "Setup zRAM", "Configure pacman repositories",
-        "Setup Chaotic-AUR", "Setup CachyOS Repository", "Quit"
-    ]
-    current_row = 0
+    menu_items = {
+        "Install Filesystem": disk_operations.install_filesystem_menu,
+        "Install essential packages": system_config.install_essential_packages,
+        "Configure fstab": disk_operations.configure_fstab,
+        "Chroot into system": utils.chroot_into_system,
+        "Set time zone": system_config.set_time_zone,
+        "Localization": system_config.localization,
+        "Network configuration": system_config.network_configuration,
+        "Set hostname": system_config.set_hostname,
+        "Set root password": system_config.set_root_password,
+        "Create a new user": system_config.create_user,
+        "Kernel Selector": system_config.kernel_selector,
+        "Install additional packages": system_config.install_additional_packages,
+        "Install custom packages": system_config.install_custom_packages,
+        "Desktop Environment Installation": system_config.install_desktop_environment,
+        #"Enable necessary services": system_config.enable_necessary_services,
+        "Setup zRAM": utils.setup_zram,
+        #"Configure pacman repositories": system_config.configure_pacman_repositories,
+        "Setup Chaotic-AUR": system_config.setup_chaotic_aur,
+        "Setup CachyOS Repository": system_config.setup_cachyos_repo,
+        #"Install Bootloader": system_config.install_bootloader,
+        #"Finish Installation": utils.finish_installation,
+        "Quit": exit
+    }
 
     curses.curs_set(0)
-    stdscr.keypad(1)  # Enable special keys
-    # Initialize color pairs for menu highlighting
+    stdscr.nodelay(1)
+    stdscr.timeout(100)
+    curses.start_color()
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
+    current_row = 0
 
     while True:
-        try:
-            stdscr.clear()
-            h, w = stdscr.getmaxyx()
+        stdscr.clear()
+        h, w = stdscr.getmaxyx()
+        for idx, item in enumerate(menu_items.keys()):
+            x = w // 2 - len(item) // 2
+            y = h // 2 - len(menu_items) // 2 + idx
+            if idx == current_row:
+                stdscr.attron(curses.color_pair(1))
+                stdscr.addstr(y, x, item)
+                stdscr.attroff(curses.color_pair(1))
+            else:
+                stdscr.addstr(y, x, item)
 
-            # Draw title
-            title = "Arch Linux Installation Menu"
-            stdscr.attron(curses.color_pair(2))
-            stdscr.addstr(1, (w - len(title)) // 2, title)
-            stdscr.attroff(curses.color_pair(2))
+        key = stdscr.getch()
 
-            # Draw menu items
-            for idx, item in enumerate(menu_items):
-                x = w // 4
-                y = h // 4 + idx
-                if idx == current_row:
-                    stdscr.attron(curses.color_pair(1))
-                    stdscr.addstr(y, x, item)
-                    stdscr.attroff(curses.color_pair(1))
-                else:
-                    stdscr.addstr(y, x, item)
-
-            # Draw footer
-            footer = "Use arrow keys to navigate, Enter to select, and 'q' to quit."
-            stdscr.addstr(h - 2, (w - len(footer)) // 2, footer)
-
-            key = stdscr.getch()
-
-            if key == curses.KEY_UP and current_row > 0:
-                current_row -= 1
-            elif key == curses.KEY_DOWN and current_row < len(menu_items) - 1:
-                current_row += 1
-            elif key == curses.KEY_ENTER or key in [10, 13]:  # Enter key
-                if current_row == 0:
-                    disk_operations.install_filesystem_menu(stdscr)
-                elif current_row == 1:
-                    system_config.install_essential_packages()
-                elif current_row == 2:
-                    disk_operations.configure_fstab()
-                elif current_row == 3:
-                    utils.chroot_into_system()
-                elif current_row == 4:
-                    system_config.set_time_zone()
-                elif current_row == 5:
-                    system_config.localization()
-                elif current_row == 6:
-                    system_config.network_configuration(stdscr)
-                elif current_row == 7:
-                    system_config.set_hostname()
-                elif current_row == 8:
-                    system_config.set_root_password()
-                elif current_row == 9:
-                    system_config.create_user()
-                elif current_row == 10:
-                    system_config.kernel_selector(stdscr)
-                elif current_row == 11:
-                    system_config.install_additional_packages(stdscr)
-                elif current_row == 12:
-                    system_config.install_custom_packages()
-                elif current_row == 13:
-                    system_config.install_desktop_environment(stdscr)
-                elif current_row == 14:
-                    system_config.enable_necessary_services()
-                elif current_row == 15:
-                    utils.setup_zram()
-                elif current_row == 16:
-                    system_config.configure_pacman_repositories()
-                elif current_row == 17:
-                    system_config.setup_chaotic_aur()
-                elif current_row == 18:
-                    system_config.setup_cachyos_repo()
-                elif current_row == len(menu_items) - 1:
-                    break
-            elif key == ord('q'):
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < len(menu_items) - 1:
+            current_row += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:  # Enter key
+            selected_function = menu_items.get(list(menu_items.keys())[current_row])
+            if selected_function:
+                selected_function(stdscr)
+            elif current_row == len(menu_items) - 1:
                 break
 
-        except OSError as e:
-            utils.handle_error(stdscr, f"OS error: {str(e)}")
-        except ValueError as e:
-            utils.handle_error(stdscr, f"Value error: {str(e)}")
+        stdscr.refresh()
 
-        stdscr.refresh()  # Refresh the screen
+    stdscr.getch()
 
-    stdscr.keypad(0)  # Disable special keys
-    curses.endwin()
+# Assuming the other required functions and imports are present in your code.
+
 
 
 # To start the menu
