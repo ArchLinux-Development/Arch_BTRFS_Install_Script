7fdd803861170ff8b8b3aa1eeee7db0fa27cad3c
Eliminater74 <eliminater74@gmail.com>
2023-09-08 12:00:36 -0400

    bootloader menu fixed

--- libs/bootloader.py
+++ libs/bootloader.py
@@ -40,28 +40,6 @@ def install_bootloader(stdscr, bootloader_choice):
         stdscr.addstr(0, 0, f"Error installing {bootloader_choice}: {str(e)}")
         stdscr.getch()
 
-def render_menu(stdscr, bootloaders, selected_idx):
-    """
-    Render the bootloader options menu.
-
-    Parameters:
-    - stdscr: the curses window object
-    - bootloaders: list of available bootloader options
-    - selected_idx: index of the currently selected bootloader option
-    """
-    stdscr.clear()
-    h, w = stdscr.getmaxyx()
-    for idx, bootloader in enumerate(bootloaders):
-        y = h // 2 - len(bootloaders) // 2 + idx
-        x = w // 2 - len(bootloader) // 2
-        # Highlight the currently selected bootloader option
-        if idx == selected_idx:
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addstr(y, x, bootloader)
-            stdscr.attroff(curses.color_pair(1))
-        else:
-            stdscr.addstr(y, x, bootloader)
-    stdscr.refresh()
 
 def handle_input(stdscr, bootloaders, selected_idx):
     """
@@ -85,6 +63,43 @@ def handle_input(stdscr, bootloaders, selected_idx):
         return selected_idx, True
     return selected_idx, False
 
+
+def render_menu(stdscr, bootloaders, selected_idx):
+    """
+    Render the bootloader options menu.
+
+    Parameters:
+    - stdscr: the curses window object
+    - bootloaders: list of available bootloader options
+    - selected_idx: index of the currently selected bootloader option
+    """
+    stdscr.clear()
+    h, w = stdscr.getmaxyx()
+
+    # Draw multi-color border
+    stdscr.hline(0, 0, curses.ACS_HLINE, curses.COLS - 1, curses.color_pair(4))
+    stdscr.vline(0, curses.COLS - 2, curses.ACS_VLINE, curses.LINES, curses.color_pair(5))
+    stdscr.hline(curses.LINES - 1, 0, curses.ACS_HLINE, curses.COLS - 1, curses.color_pair(6))
+    stdscr.vline(0, 0, curses.ACS_VLINE, curses.LINES, curses.color_pair(7))
+    stdscr.addch(0, 0, curses.ACS_ULCORNER, curses.color_pair(4))
+    stdscr.addch(0, curses.COLS - 2, curses.ACS_URCORNER, curses.color_pair(5))
+    stdscr.addch(curses.LINES - 1, curses.COLS - 2, curses.ACS_LRCORNER, curses.color_pair(6))
+    stdscr.addch(curses.LINES - 1, 0, curses.ACS_LLCORNER, curses.color_pair(7))
+
+    for idx, bootloader in enumerate(bootloaders):
+        y = h // 2 - len(bootloaders) // 2 + idx
+        x = w // 2 - len(bootloader) // 2
+        # Highlight the currently selected bootloader option
+        if idx == selected_idx:
+            stdscr.attron(curses.color_pair(1))
+            stdscr.addstr(y, x, bootloader)
+            stdscr.attroff(curses.color_pair(1))
+        else:
+            stdscr.addstr(y, x, bootloader)
+    stdscr.refresh()
+
+# ... [rest of the functions]
+
 def bootloader_menu(stdscr):
     """
     Display an interactive menu for the user to choose a bootloader.
@@ -92,6 +107,14 @@ def bootloader_menu(stdscr):
     Parameters:
     - stdscr: the curses window object
     """
+    # Initialize color pairs
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)  # Selected menu item
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)   # Header/Footer
+    curses.init_pair(4, curses.COLOR_RED, curses.COLOR_BLACK)    # Top border
+    curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Right border
+    curses.init_pair(6, curses.COLOR_BLUE, curses.COLOR_BLACK)   # Bottom border
+    curses.init_pair(7, curses.COLOR_YELLOW, curses.COLOR_BLACK) # Left border
+
     bootloaders = ['GRUB', 'rEFInd', 'systemd-boot', 'Exit']  # Available bootloader options
     selected_idx = 0
     option_selected = False
